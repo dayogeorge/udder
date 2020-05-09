5fbd32e903b7b99ee87186d035d393d2bedcfdbb
oladayo george <oladayogeorge17@gmail.com>
2020-05-09 07:51:21 +0100

    first logo and title

---
+++ udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  udder
+//
+//  Created by Oladayo George on 08/05/2020.
+//  Copyright © 2020 Oladayo George. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
